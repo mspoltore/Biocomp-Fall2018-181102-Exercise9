2be9b21c89ed39e6109368ce7b9cfde7d6dcea9f
Michael Spoltore <michaelspoltore@michaels-mbp.dhcp.nd.edu>
2018-11-07 20:53:54 -0500

    Finished first part of question 2

--- exercise9.R
+++ exercise9.R
@@ -2,7 +2,6 @@
 
 rm(list=ls())
 setwd("~/Desktop/biocomp2018/exercise9/Biocomp-Fall2018-181102-Exercise9")
-data=read.csv("data.txt", header = TRUE, stringsAsFactors = FALSE)
 
 # Part 1
 BMI=read.csv("BMI.txt", header = TRUE, stringsAsFactors = FALSE)
@@ -19,4 +18,21 @@ head(BMI)
 a=ggplot(data=BMI,aes(x=BMIdiff,y=weightDiff))
 a+geom_point()+theme_classic()+stat_smooth(method="lm")
 
+# Part 2
+data=read.csv("data.txt", header = TRUE, stringsAsFactors = FALSE)
+
+northData=(data[data$region == "north",2])
+northMean = mean(northData)
+eastData=(data[data$region == "east",2])
+eastMean = mean(eastData)
+southData=(data[data$region == "south",2])
+southMean = mean(southData)
+westData=(data[data$region == "west",2])
+westMean = mean(westData)
+
+dirMeans = data.frame(Region=c("northMean","southMean","eastMean","westMean"), Mean=c(northMean,southMean,eastMean,westMean))
+dirMeans
+means=c(northMean,southMean,eastMean,westMean)
+b=ggplot(data=dirMeans, aes(x=Region,y=Mean))
+b+geom_bar(stat="identity")
 
